3e70a647c7f806095939011c82ec79fd789ca83e
Emirlan <you@example.com>
2022-07-13 17:50:59 +0600

    emir

--- config/main/models.py
+++ config/main/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 
-# Create your models here.
-# hello world
+class Credit_Special(models.Model):
+    full_name = models.CharField(max_length=100)
+    job_title = models.ForeignKey('Job_titl', on_delete=models.CASCADE)
+    #id_klient = models.ForeignKey(ContrAgent, on_delete=models.CASCADE)
+
+class Job_titl(models.Model):
+    name_job_title = models.CharField(max_length=100)
+
+
+class Guarantor(models.Model):
+    #id_guarantor= models.ForeignKey('Job_titl', on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+
+
+
+
+
+
+
+
+    full_status =
+    Credit_history = models.CharField(max_length=200)
